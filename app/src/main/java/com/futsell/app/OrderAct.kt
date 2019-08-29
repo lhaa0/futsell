1e2445431286175c6daceb17480fce7acad73071
elysunariah <ellysunariah255@gmail.com>
2019-08-29 13:42:04 +0700

    list

---
+++ app/src/main/java/com/futsell/app/OrderAct.kt
@@ -0,0 +1,16 @@
+package com.futsell.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.common.util.CollectionUtils.listOf
+
+class OrderAct : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.order_act)
+
+    }
+
+
+}
